3799b16d2400971585839781f21a9995c3920b59
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2022-11-27 21:54:44 +0000

    adicionei menu de escolha de ficheiros

--- main.py
+++ main.py
@@ -7,7 +7,28 @@ from graph import graph
 import time
 
 def main():
-    file = open("circuito3.txt")
+    circuito = "circuito1.txt"
+    print("Escolha o circuito que pretende usar:\n")
+    cont = -1
+    while cont < 0:
+        print("1 - Circuito 1 (Igual ao do enunciado)")
+        print("2 - Circuito 2")
+        print("3 - Circuito 3\n")
+
+        cont = int(input("Introduza a sua opção -> "))
+
+        if cont == 1:
+            circuito = "circuito1.txt"
+        elif cont == 2:
+            circuito = "circuito2.txt"
+        elif cont == 3:
+            circuito = "circuito3.txt"
+        else:
+            print("Opção inválida...")
+            l = input("Prima enter para continuar")
+
+    print(" L O A D I N G . . .")
+    file = open(circuito)
     y = 0
     b = []
     partida = (0,0)
@@ -26,40 +47,40 @@ def main():
         y += 1
 
     g = graph(partida,fim,b)
-    print(b)
-    print("fim: ",fim,"\n")
-    print("partida:",partida,"\n")
+    # print(b)
+    print("Partida:", partida, "\n")
+    print("Fim: ", fim, "\n")
     path = g.AEstrela()
     #cosntrução de menu
     saida = -1
     while saida != 0:
-        print("1-Imprimir Grafo do circuito")
-        print("2-Desenhar circuito em VectorRace")
-        print("3-Resolver o problema utilizando um algoritmo")
-        print("0-Saír")
+        print("1 - Imprimir Grafo do circuito")
+        print("2 - Desenhar circuito em VectorRace")
+        print("3 - Resolver o problema utilizando um algoritmo")
+        print("0 - Saír")
 
-        saida = int(input("introduza a sua opcao-> "))
+        saida = int(input("Introduza a sua opção -> "))
         if saida == 0:
-            print("saindo.......")
+            print("Saindo.......")
         elif saida == 1:
             #Escrever o grafo como string
             print(g)
             time.sleep(1)
             g.desenha()
-            l=input("prima enter para continuar")
+            l=input("Prima enter para continuar")
         elif saida == 2:
             #Desenhar o grafo de forma gráfica
             janela = pygame.display.set_mode((40*len(b), 15 * len(b[0])))
             g.plot(janela,path)
-            l=input("prima enter para continuar")
+            l=input("Prima enter para continuar")
             pygame.quit()
         elif saida == 3:
             #Imprimir as chaves do dicionario que representa o grafo
             print("Resposta do Algoritmo AEstrela: " + str(g.AEstrela()))
-            l = input("prima enter para continuar")
+            l = input("Prima enter para continuar")
         else:
             print("Opção inválida...")
-            l = input("prima enter para continuar")
+            l = input("Prima enter para continuar")
 
 
 
