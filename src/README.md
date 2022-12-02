265cd2d6c609d91590481d3916fffb211d683a78
Bernard A. Georges <73658047+BernardMatulo@users.noreply.github.com>
2022-12-02 18:27:59 +0000

    fase 2
    mudei tudo nao sei explicar muito bem mas mudei

---
+++ src/README.md
@@ -0,0 +1,19 @@
+# AI Grupo4
+
+a96326 - Bernard Ambrosio Georges 
+
+a94870 - Rafael Picão Ferreira Correia
+
+a91682 - Robert Beniamin Szabo
+
+a97257 - João Miguel Ferreira Loureiro
+
+To Do:    
+Parte 1:   
+    Relatorio   
+Parte 2:   
+    Fazer o BFS   
+    Fazer o DFS   
+    Fazer multibots   
+    Apresentar o caminho percorrido ao longo da
+execução em cada um dos algoritmos desenvolvidos.
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #Inteligência Artificial
 #2022/23
 import pygame
-import os.path, sys
 
+from algoritmos import algoritmos
 #Importar classe graph
 from graph import graph
 import time
@@ -28,15 +28,15 @@ def main():
         elif cont == 3:
             circuito = "circuito3.txt"
         elif cont == 4:
-            circuito = "circuito4.txt"
+            circuito = "circuitoBasic.txt"
         elif cont == 5:
             print("Por favor introduza o path do circuito")
             circuito = input()
         else:
             print("Opção inválida...")
             l = input("Prima enter para continuar")
-        try:
-            file = open(os.path.join("maps", circuito))
+        try: 
+            file = open(circuito)
         except:
             print("\nFICHEIRO INVÁLIDO")
             print("Por favor introduza corretamente")
@@ -45,7 +45,7 @@ def main():
     print(" L O A D I N G . . .")
     y = 0
     b = []
-    partida = (0,0)
+    partida = []
     fim = []
     for line in file:
         x = 0
@@ -54,31 +54,21 @@ def main():
             if char != ' ' and char != '\n':
                 b[y].append(char)
                 if char == 'P':
-                    partida = (x,y)
+                    partida.append((x,y))
                 if char == 'F':
                     fim.append((x,y))
                 x += 1
         y += 1
 
-    g = graph(partida,fim,b)
+    g = graph(partida[0] ,b)
+    print(g.estadoPossivel((9, 3)))
+    algs = algoritmos(g,partida[0],fim)
     # print(b)
     print("Partida:", partida, "\n")
     print("Fim: ", fim, "\n")
+    path = algs.AEstrela()
     #cosntrução de menu
     saida = -1
-    while saida != 1 and saida!=2:
-        print("1 - BFS")
-        print("2 - AEstrela")
-
-        saida = int(input("Introduza a sua opção -> "))
-        if saida == 1:
-            path = g.procura_BFS()
-        elif saida == 2:
-            path = g.AEstrela()
-        else:
-            print("Opção inválida...")
-            l = input("Prima enter para continuar")
-    saida=-1
     while saida != 0:
         print("1 - Imprimir Grafo do circuito")
         print("2 - Desenhar circuito em VectorRace")
@@ -96,14 +86,13 @@ def main():
             l=input("Prima enter para continuar")
         elif saida == 2:
             #Desenhar o grafo de forma gráfica
-            janela = pygame.display.set_mode((80*len(b), 15 * len(b[0])))
+            janela = pygame.display.set_mode((30*len(b[0]), 30 * len(b)))
             g.plot(janela,path)
             l=input("Prima enter para continuar")
             pygame.quit()
         elif saida == 3:
             #Imprimir as chaves do dicionario que representa o grafo
-            print("Resposta do Algoritmo: " + str(path))
-            print("Custo Total: " + str(g.custoFinal(path)))
+            print("Resposta do Algoritmo AEstrela: " + str(algs.AEstrela()))
             l = input("Prima enter para continuar")
         else:
             print("Opção inválida...")
