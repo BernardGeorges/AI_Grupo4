75cb03a16c45692256f400fb78c6aba49466338d
Bernard A. Georges <73658047+BernardMatulo@users.noreply.github.com>
2022-12-21 23:17:11 +0000

    one less feature

--- src/algoritmos.py
+++ src/algoritmos.py
@@ -59,8 +59,8 @@ class algoritmos:
                 if min(estadoInicial[0][0],estadoFinal[0][0]) <= x <= max(estadoInicial[0][0],estadoFinal[0][0]) and min(estadoInicial[0][1],estadoFinal[0][1]) <= y <= max(estadoInicial[0][1],estadoFinal[0][1]) and min(path[play-1][0][0], path[play][0][0]) <= x <= max(path[play-1][0][0], path[play][0][0]) and min(path[play-1][0][1], path[play][0][1]) <= y <= max(path[play-1][0][1], path[play][0][1]):
                     distanciaPlayer0 = self.graph.calcBestHeuristica([((x,y),estadoInicial[1])],[estadoFinal[0]],False)
                     distanciaPlayer1 = self.graph.calcBestHeuristica([((x,y),estadoInicial[1])],[path[play][0]],False)
-                    speedPlayer0 = math.sqrt((estadoInicial[0][1]^2) + (estadoInicial[0][0]^2))
-                    speedPlayer1 = math.sqrt((path[play-1][0][1]^2) + (path[play-1][0][0]^2))
+                    speedPlayer0 = math.sqrt(pow(estadoFinal[1][1],2) + pow(estadoFinal[1][0],2))
+                    speedPlayer1 = math.sqrt(pow(path[play][1][1],2) + pow(path[play][1][0],2))
                     tempoPlayer0 = round(distanciaPlayer0/speedPlayer0,1)
                     tempoPlayer1 = round(distanciaPlayer1/speedPlayer1,1)
                     return self.matrix[nodoPlayer[1]][nodoPlayer[0]] != str(play) and tempoPlayer0 != tempoPlayer1
